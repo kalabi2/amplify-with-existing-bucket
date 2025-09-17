885960db17d5eafd233f3044091d817065aade88
kalabi <kalabi@deloitte.ie>
2025-09-17 17:13:00 +0100

    adding existing bucket

--- src/main.ts
+++ src/main.ts
@@ -6,23 +6,7 @@ Amplify.configure({
       userPoolId: "eu-west-1_0bIJhzB7g",
       userPoolClientId: "7ahg3bv8gaqs0tv0bln910mpf3",
       identityPoolId: "eu-west-1:788c7171-e74c-48f3-b2e6-d393c565dea9",
-      loginWith: {
-        email: true,
-      },
-      signUpVerificationMethod: "code",
-      userAttributes: {
-        email: {
-          required: true,
-        },
-      },
-      allowGuestAccess: true,
-      passwordFormat: {
-        minLength: 8,
-        requireLowercase: true,
-        requireUppercase: true,
-        requireNumbers: true,
-        requireSpecialCharacters: true,
-      },
+      authenticationFlowType: 'USER_SRP_AUTH',
     
   },
 })
