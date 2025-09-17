b6f7cce97e6c5ad567fb72b90af4ce81d802b872
kalabi <kalabi@deloitte.ie>
2025-09-17 16:05:18 +0100

    adding existing bucket

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { Amplify } from 'aws-amplify';
-
-Amplify.configure({
-  Auth: {
-    Cognito: {
-      userPoolId: 'eu-west-1_0bIJhzB7g',
-      userPoolClientId: '7ahg3bv8gaqs0tv0bln910mpf3',
-      identityPoolId: 'eu-west-1:788c7171-e74c-48f3-b2e6-d393c565dea9',
-      loginWith: {
-        email: true,
-      },
-      signUpVerificationMethod: "code",
-      userAttributes: {
-        email: {
-          required: true,
-        },
-      },
-      allowGuestAccess: true,
-      passwordFormat: {
-        minLength: 8,
-        requireLowercase: true,
-        requireUppercase: true,
-        requireNumbers: true,
-        requireSpecialCharacters: true,
-      },
-    },
-  },
-});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
