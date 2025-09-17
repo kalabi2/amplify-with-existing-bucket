2224f0c2fcd53020fbe923dcf8e9b6f7a013c51c
kalabi <kalabi@deloitte.ie>
2025-09-17 16:24:24 +0100

    adding existing bucket

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,9 +1,12 @@
-import { referenceAuth } from '@aws-amplify/backend';
+import { defineAuth } from '@aws-amplify/backend';
 
-export const auth = referenceAuth({
-  userPoolId: 'eu-west-1_0bIJhzB7g',
-  identityPoolId: 'eu-west-1:788c7171-e74c-48f3-b2e6-d393c565dea9',
-  authRoleArn: 'arn:aws:iam::484907493866:role/service-role/amplify-identity-pool-role',
-  unauthRoleArn: 'arn:aws:iam::484907493866:role/service-role/amplify-identity-pool-role',
-  userPoolClientId: '7ahg3bv8gaqs0tv0bln910mpf3',
+/**
+ * Define and configure your auth resource
+ * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ */
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+  },
+  groups: ['admin']
 });
