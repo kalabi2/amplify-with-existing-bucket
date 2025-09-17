0aa7d1468c2b32ca880f94659779af7497c26cb9
kalabi <kalabi@deloitte.ie>
2025-09-17 15:27:31 +0100

    adding existing bucket

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,8 @@ Amplify.configure({
   Auth: {
     Cognito: {
       userPoolId: 'eu-west-1_0bIJhzB7g',
-      userPoolWebClientId: '5gq7rtkmks8vkribkieebmdsqo',
-      identityPoolId: 'eu-west-1:788c7171-e74c-48f3-b2e6-d393c565dea9', 
-      region: 'eu-west-1',
+      userPoolClientId: '5gq7rtkmks8vkribkieebmdsqo',
+      identityPoolId: 'eu-west-1:788c7171-e74c-48f3-b2e6-d393c565dea9',
       loginWith: {
         email: true,
       },
