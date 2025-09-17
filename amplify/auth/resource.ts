df3106595eaffa8e31d5b848ff24423afd12aebc
kalabi <kalabi@deloitte.ie>
2025-09-17 18:30:30 +0100

    adding existing bucket

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,15 +1,4 @@
-// import { defineAuth } from '@aws-amplify/backend';
 
-// /**
-//  * Define and configure your auth resource
-//  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
-//  */
-// export const auth = defineAuth({
-//   loginWith: {
-//     email: true,
-//   },
-//   groups: ['admin']
-// });
 
 import { referenceAuth } from '@aws-amplify/backend';
 
