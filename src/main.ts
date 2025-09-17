f53bea774521035319b83fcb6ae7d42f02ae10fb
kalabi <kalabi@deloitte.ie>
2025-09-17 17:18:23 +0100

    adding existing bucket

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { Amplify } from "aws-amplify"
 
 Amplify.configure({
   Auth: {
+    Cognito: {
       userPoolId: "eu-west-1_0bIJhzB7g",
       userPoolClientId: "7ahg3bv8gaqs0tv0bln910mpf3",
       identityPoolId: "eu-west-1:788c7171-e74c-48f3-b2e6-d393c565dea9",
       authenticationFlowType: 'USER_SRP_AUTH',
-    
+    },
   },
 })
