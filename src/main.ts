ee7f1530af6f77085a6bc6128ebb729b1c8b7887
kalabi <kalabi@deloitte.ie>
2025-09-17 16:31:28 +0100

    adding existing bucket

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { Amplify } from "aws-amplify"
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: "eu-west-1_0bIJhzB7g",
+      userPoolClientId: "5gq7rtkmks8vkribkieebmdsqo",
+      identityPoolId: "eu-west-1:788c7171-e74c-48f3-b2e6-d393c565dea9",
+      loginWith: {
+        email: true,
+      },
+      signUpVerificationMethod: "code",
+      userAttributes: {
+        email: {
+          required: true,
+        },
+      },
+      allowGuestAccess: true,
+      passwordFormat: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireNumbers: true,
+        requireSpecialCharacters: true,
+      },
+    },
+  },
+})
