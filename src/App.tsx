7f66c1b8854688c54d77a84e56258776be808bf9
kalabi <kalabi@deloitte.ie>
2025-09-18 15:57:10 +0100

    original

--- src/App.tsx
+++ src/App.tsx
@@ -4,53 +4,29 @@ import {
 } from '@aws-amplify/ui-react-storage/browser';
 import '@aws-amplify/ui-react-storage/styles.css';
 import './App.css';
-import Background from './Modern.jpg';
+
 import config from '../amplify_outputs.json';
 import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-import { Authenticator, Button, Heading } from '@aws-amplify/ui-react';
-
-// Configure Amplify in index file or root file
-const amplifyConfig = {
-  ...awsExports,
-  ...config,
-};
-
-
-Amplify.configure(amplifyConfig);
+import { Authenticator, Button, Flex, Heading } from '@aws-amplify/ui-react';
+Amplify.configure(config);
 
 const { StorageBrowser } = createStorageBrowser({
   config: createAmplifyAuthAdapter(),
 });
 
 function App() {
-  const appStyle = {
-    width: '100vw',
-    height: '100vh',
-    backgroundImage: `url(${Background})`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-  };
-
   return (
-    <div style={appStyle}>
-      <Authenticator hideSignUp>
+    <Authenticator hideSignUp>
         {({ signOut }) => (
           <>
-            <div className="header">
+             <Flex direction="row" alignItems="center" wrap="nowrap" gap="1rem">
               <Heading level={4}>CSO AWS Storage Browser Portal</Heading>
-              <Button onClick={signOut} variation="primary">
-                Sign out
-              </Button>
-            </div>
-            <div style={{ padding: '0 2rem' }}>
+              <Button onClick={signOut}>Sign out</Button>
+            </Flex>
               <StorageBrowser />
-            </div>
           </>
         )}
       </Authenticator>
-    </div>
   );
 }
 
