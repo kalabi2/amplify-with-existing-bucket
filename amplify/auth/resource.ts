8e417bc436fc2ac2bf088a9953eddefabc73c419
kalabi <kalabi@deloitte.ie>
2025-09-22 16:49:15 +0100

    adding existing bucket

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -18,6 +18,6 @@ export const auth = referenceAuth({
   userPoolId: 'eu-west-1_zBJNBJaTu',
   identityPoolId: 'eu-west-1:4b6c1042-d067-4b25-87a5-b79f39079101',
   authRoleArn: 'arn:aws:iam::484907493866:role/amplify-d1udrw7f3bvbsj-ma-amplifyAuthauthenticatedU-5ixSVfoNXNfT',
-  unauthRoleArn: 'arn:aws:iam::484907493866:role/amplify-d1udrw7f3bvbsj-ma-amplifyAuthauthenticatedU-5ixSVfoNXNfT',
+  unauthRoleArn: 'arn:aws:iam::484907493866:role/amplify-d1udrw7f3bvbsj-ma-amplifyAuthunauthenticate-rxjndeEn5tSk',
   userPoolClientId: '6nqembempmhv6j9tjj4jpsotui',
 });
