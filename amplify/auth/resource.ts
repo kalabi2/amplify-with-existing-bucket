d261cf9c840f77702be26e5d36933b8deeaf1db6
kalabi <kalabi@deloitte.ie>
2025-09-22 17:09:03 +0100

    adding existing bucket

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,23 +1,23 @@
-// import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth } from '@aws-amplify/backend';
 
-// /**
-//  * Define and configure your auth resource
-//  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
-//  */
-// export const auth = defineAuth({
-//   loginWith: {
-//     email: true,
-//   },
-//   groups: ['admin']
-// });
-
-
-import { referenceAuth } from '@aws-amplify/backend';
-
-export const auth = referenceAuth({
-  userPoolId: 'eu-west-1_0bIJhzB7g',
-  identityPoolId: 'eu-west-1:788c7171-e74c-48f3-b2e6-d393c565dea9',
-  authRoleArn: 'arn:aws:iam::484907493866:role/amplify-d1udrw7f3bvbsj-ma-amplifyAuthauthenticatedU-5ixSVfoNXNfT',
-  unauthRoleArn: 'arn:aws:iam::484907493866:role/amplify-d1udrw7f3bvbsj-ma-amplifyAuthauthenticatedU-5ixSVfoNXNfT',
-  userPoolClientId: '7ahg3bv8gaqs0tv0bln910mpf3',
+/**
+ * Define and configure your auth resource
+ * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ */
+export const auth = defineAuth({
+  loginWith: {
+    email: true,
+  },
+  groups: ['admin']
 });
+
+
+// import { referenceAuth } from '@aws-amplify/backend';
+
+// export const auth = referenceAuth({
+//   userPoolId: 'eu-west-1_0bIJhzB7g',
+//   identityPoolId: 'eu-west-1:788c7171-e74c-48f3-b2e6-d393c565dea9',
+//   authRoleArn: 'arn:aws:iam::484907493866:role/amplify-d1udrw7f3bvbsj-ma-amplifyAuthauthenticatedU-5ixSVfoNXNfT',
+//   unauthRoleArn: 'arn:aws:iam::484907493866:role/amplify-d1udrw7f3bvbsj-ma-amplifyAuthauthenticatedU-5ixSVfoNXNfT',
+//   userPoolClientId: '7ahg3bv8gaqs0tv0bln910mpf3',
+// });
