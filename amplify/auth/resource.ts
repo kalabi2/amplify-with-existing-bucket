2e91a0725c546846ef21c81ae1830d2670829325
kalabi <kalabi@deloitte.ie>
2025-09-23 12:08:39 +0100

    using existing cognito

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -15,10 +15,10 @@
 import { referenceAuth } from '@aws-amplify/backend';
 
 export const auth = referenceAuth({
-  userPoolId: 'eu-west-1_Vnb6T8L0Z',
+  userPoolId: 'eu-west-1_jEOQnBdSL',
   identityPoolId: 'eu-west-1:788c7171-e74c-48f3-b2e6-d393c565dea9',
   authRoleArn: 'arn:aws:iam::484907493866:role/service-role/amplify-identity-pool-role',
   unauthRoleArn: 'arn:aws:iam::484907493866:role/service-role/amplify-identity-pool-role',
-  userPoolClientId: '4rj7jni406iha44qubt4u7vape',
+  userPoolClientId: '38jldpipndtt2obmpdahdb139',
   
 });
